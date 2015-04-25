46b406ec3d538a886df44a44a5ef15e43febe9db
Paul S. Chun <paul@rivalfox.com>
2015-04-25 16:01:30 +0200

    update production url to reflect new route

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://ghost-o.rivalfox.com',
+        url: 'http://haus.rivalfox.com/blog',
         mail: {},
         database: {
             client: 'mysql',
